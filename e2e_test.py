57a557fc3ef6b6eb9c6f8faf07ab9ae5c553b910
hitsjt <hitsjt@users.noreply.github.com>
2021-07-06 17:58:38 +0800

    Update: coalab_git.ipynb e2e_test.py nbdime_test.ipynb notebook_git.ipynb test

---
+++ e2e_test.py
@@ -0,0 +1,203 @@
+# Databricks notebook source
+import pandas as pd
+
+white_wine = pd.read_csv("/dbfs/FileStore/shared_uploads/jiangtaosun@microsoft.com/winequality_white.csv", sep=';')
+red_wine = pd.read_csv("/dbfs/FileStore/shared_uploads/jiangtaosun@microsoft.com/winequality_red.csv", sep=';')
+
+# COMMAND ----------
+
+red_wine['is_red'] = 1
+white_wine['is_red'] = 0
+ 
+data = pd.concat([red_wine, white_wine], axis=0)
+data.head()
+
+# COMMAND ----------
+
+data.rename(columns=lambda x: x.replace(' ', '_'), inplace=True)
+data.head()
+
+# COMMAND ----------
+
+import seaborn as sns
+sns.distplot(data.quality, kde=False)
+
+# COMMAND ----------
+
+high_quality = (data.quality >= 7).astype(int)
+data.quality = high_quality
+
+# COMMAND ----------
+
+import matplotlib.pyplot as plt
+ 
+dims = (3, 4)
+ 
+f, axes = plt.subplots(dims[0], dims[1], figsize=(25, 15))
+axis_i, axis_j = 0, 0
+for col in data.columns:
+  if col == 'is_red' or col == 'quality':
+    continue # Box plots cannot be used on indicator variables
+  sns.boxplot(x=high_quality, y=data[col], ax=axes[axis_i, axis_j])
+  axis_j += 1
+  if axis_j == dims[1]:
+    axis_i += 1
+    axis_j = 0
+
+# COMMAND ----------
+
+data.isna().any()
+
+# COMMAND ----------
+
+from sklearn.model_selection import train_test_split
+ 
+train, test = train_test_split(data, random_state=123)
+X_train = train.drop(["quality"], axis=1)
+X_test = test.drop(["quality"], axis=1)
+y_train = train.quality
+y_test = test.quality
+
+# COMMAND ----------
+
+import mlflow
+import mlflow.pyfunc
+import mlflow.sklearn
+import numpy as np
+import sklearn
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import roc_auc_score
+from mlflow.models.signature import infer_signature
+from mlflow.utils.environment import _mlflow_conda_env
+import cloudpickle
+import time
+ 
+# The predict method of sklearn's RandomForestClassifier returns a binary classification (0 or 1). 
+# The following code creates a wrapper function, SklearnModelWrapper, that uses 
+# the predict_proba method to return the probability that the observation belongs to each class. 
+ 
+class SklearnModelWrapper(mlflow.pyfunc.PythonModel):
+  def __init__(self, model):
+    self.model = model
+    
+  def predict(self, context, model_input):
+    return self.model.predict_proba(model_input)[:,1]
+ 
+# mlflow.start_run creates a new MLflow run to track the performance of this model. 
+# Within the context, you call mlflow.log_param to keep track of the parameters used, and
+# mlflow.log_metric to record metrics like accuracy.
+with mlflow.start_run(run_name='untuned_random_forest'):
+  n_estimators = 10
+  model = RandomForestClassifier(n_estimators=n_estimators, random_state=np.random.RandomState(123))
+  model.fit(X_train, y_train)
+ 
+  # predict_proba returns [prob_negative, prob_positive], so slice the output with [:, 1]
+  predictions_test = model.predict_proba(X_test)[:,1]
+  auc_score = roc_auc_score(y_test, predictions_test)
+  mlflow.log_param('n_estimators', n_estimators)
+  # Use the area under the ROC curve as a metric.
+  mlflow.log_metric('auc', auc_score)
+  wrappedModel = SklearnModelWrapper(model)
+  # Log the model with a signature that defines the schema of the model's inputs and outputs. 
+  # When the model is deployed, this signature will be used to validate inputs.
+  signature = infer_signature(X_train, wrappedModel.predict(None, X_train))
+  
+  # MLflow contains utilities to create a conda environment used to serve models.
+  # The necessary dependencies are added to a conda.yaml file which is logged along with the model.
+  conda_env =  _mlflow_conda_env(
+        additional_conda_deps=None,
+        additional_pip_deps=["cloudpickle=={}".format(cloudpickle.__version__), "scikit-learn=={}".format(sklearn.__version__)],
+        additional_conda_channels=None,
+    )
+  mlflow.pyfunc.log_model("random_forest_model", python_model=wrappedModel, conda_env=conda_env, signature=signature)
+
+# COMMAND ----------
+
+feature_importances = pd.DataFrame(model.feature_importances_, index=X_train.columns.tolist(), columns=['importance'])
+feature_importances.sort_values('importance', ascending=False)
+
+# COMMAND ----------
+
+run_id = mlflow.search_runs(filter_string='tags.mlflow.runName = "untuned_random_forest"').iloc[0].run_id
+
+# COMMAND ----------
+
+model_name = "wine_quality"
+model_version = mlflow.register_model(f"runs:/{run_id}/random_forest_model", model_name)
+time.sleep(15)
+
+# COMMAND ----------
+
+from mlflow.tracking import MlflowClient
+ 
+client = MlflowClient()
+client.transition_model_version_stage(
+  name=model_name,
+  version=model_version.version,
+  stage="Production",
+)
+
+# COMMAND ----------
+
+model = mlflow.pyfunc.load_model(f"models:/{model_name}/production")
+ 
+# Sanity-check: This should match the AUC logged by MLflow
+print(f'AUC: {roc_auc_score(y_test, model.predict(X_test))}')
+
+# COMMAND ----------
+
+from hyperopt import fmin, tpe, hp, SparkTrials, Trials, STATUS_OK
+from hyperopt.pyll import scope
+from math import exp
+import mlflow.xgboost
+import numpy as np
+import xgboost as xgb
+ 
+search_space = {
+  'max_depth': scope.int(hp.quniform('max_depth', 4, 100, 1)),
+  'learning_rate': hp.loguniform('learning_rate', -3, 0),
+  'reg_alpha': hp.loguniform('reg_alpha', -5, -1),
+  'reg_lambda': hp.loguniform('reg_lambda', -6, -1),
+  'min_child_weight': hp.loguniform('min_child_weight', -1, 3),
+  'objective': 'binary:logistic',
+  'seed': 123, # Set a seed for deterministic training
+}
+ 
+def train_model(params):
+  # With MLflow autologging, hyperparameters and the trained model are automatically logged to MLflow.
+  mlflow.xgboost.autolog()
+  with mlflow.start_run(nested=True):
+    train = xgb.DMatrix(data=X_train, label=y_train)
+    test = xgb.DMatrix(data=X_test, label=y_test)
+    # Pass in the test set so xgb can track an evaluation metric. XGBoost terminates training when the evaluation metric
+    # is no longer improving.
+    booster = xgb.train(params=params, dtrain=train, num_boost_round=1000,\
+                        evals=[(test, "test")], early_stopping_rounds=50)
+    predictions_test = booster.predict(test)
+    auc_score = roc_auc_score(y_test, predictions_test)
+    mlflow.log_metric('auc', auc_score)
+ 
+    signature = infer_signature(X_train, booster.predict(train))
+    mlflow.xgboost.log_model(booster, "model", signature=signature)
+    
+    # Set the loss to -1*auc_score so fmin maximizes the auc_score
+    return {'status': STATUS_OK, 'loss': -1*auc_score, 'booster': booster.attributes()}
+ 
+# Greater parallelism will lead to speedups, but a less optimal hyperparameter sweep. 
+# A reasonable value for parallelism is the square root of max_evals.
+spark_trials = SparkTrials(parallelism=10)
+ 
+# Run fmin within an MLflow run context so that each hyperparameter configuration is logged as a child run of a parent
+# run called "xgboost_models" .
+with mlflow.start_run(run_name='xgboost_models'):
+  best_params = fmin(
+    fn=train_model, 
+    space=search_space, 
+    algo=tpe.suggest, 
+    max_evals=96,
+    trials=spark_trials, 
+    rstate=np.random.RandomState(123)
+  )
+
+# COMMAND ----------
+
